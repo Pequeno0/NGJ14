f5ee4739de54a1776dc63a4266779a566439aaaf
robintheilade <robin.theilade@gmail.com>
2014-02-15 00:50:11 +0100

    Added initial map generator based on map chunks

---
+++ Unity/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator
+{
+    public static readonly MapGenerator Singleton = new MapGenerator();
+
+    public IEnumerator Generate(int horiontalChunkCount, int verticalChuckCount, int chunkWidth, int chunkHeight, params string[] availableChunkNames)
+    {
+        for (var y = 0; y < verticalChuckCount; y++)
+        {
+            for (var x = 0; x < horiontalChunkCount; x++)
+            {
+                var rnd = Random.Range(0, availableChunkNames.Length);
+                var chunkName = availableChunkNames[rnd];
+                Application.LoadLevelAdditive(chunkName);
+                yield return null;
+                var chunk = GameObject.Find(chunkName);
+                chunk.name = string.Concat("Level chunk (", x, ", ", y, " [", chunk.name, "])");
+                chunk.transform.position = new Vector3(x * chunkWidth, y * chunkHeight, 0.0f);
+            }
+        }
+    }
+}
