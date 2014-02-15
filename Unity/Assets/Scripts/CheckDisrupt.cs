7fa6909ff72d9ff182777e2bc82cf70e155e0ec9
Peter Madsen <peter_f_madsen@hotmail.com>
2014-02-16 00:45:20 +0100

    false!

--- Unity/Assets/Scripts/CheckDisrupt.cs
+++ Unity/Assets/Scripts/CheckDisrupt.cs
@@ -79,8 +79,8 @@ public partial class TradingController : SingletonMonoBehaviour<TradingControlle
                     NetworkMessageController.AddToPlayerScoreOnServer(trade.InitiaterPlayer.NetworkPlayer, -1);
                     NetworkMessageController.AddToPlayerScoreOnServer(thisPlayer.NetworkPlayer, 1);
                     NetworkMessageController.StopTradingFromServer(trade);
-                    trade.InitiaterPed.IsTrading = true;
-                    trade.OtherPed.IsTrading = true;
+                    trade.InitiaterPed.IsTrading = false;
+                    trade.OtherPed.IsTrading = false;
                     //trades.Remove(trade);
                     return true;
                 }
