f75c2fc498c9cc84f26e9635cfa604ad4010d25d
robintheilade <robin.theilade@gmail.com>
2014-02-15 17:48:40 +0100

    Added very early non working trade code to make room for a git pull

---
+++ Unity/Assets/Scripts/TradePair.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TradePair
+{
+    public GameObject Initiater
+    {
+        get;
+        set;
+    }
+
+    public GameObject Other
+    {
+        get;
+        set;
+    }
+
+    public float StartTime
+    {
+        get;
+        set;
+    }
+
+    public float Length
+    {
+        get;
+        set;
+    }
+}
---
+++ Unity/Assets/Scripts/TradingController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TradingController : SingletonMonoBehaviour<TradingController>
+{
+    private readonly List<TradePair> trades = new List<TradePair>();
+
+    public void Trade(GameObject initiater, GameObject other)
+    {
+        if (this.trades.Any(t => (t.Initiater == initiater && t.Other == other) || (t.Other == initiater && t.Initiater == other)))
+        {
+            return;
+        }
+
+        var trade = new TradePair()
+        {
+            Initiater = initiater,
+            Other = other,
+            StartTime = Time.time,
+            Length = 5.0f,
+        };
+
+        this.trades.Add(trade);
+    }
+}
