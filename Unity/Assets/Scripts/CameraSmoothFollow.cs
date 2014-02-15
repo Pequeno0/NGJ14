3ca1c637af0742465b668536413d24feac8616bb
robintheilade <robin.theilade@gmail.com>
2014-02-15 16:23:36 +0100

    Added smooth follow camera

---
+++ Unity/Assets/Scripts/CameraSmoothFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+
+public class CameraSmoothFollow : BaseMonoBehaviour
+{
+    private void Update()
+    {
+        if (this.GameStateController.CurrentGameState == GameState.Playing)
+        {
+            var player = this.PlayerController.Players.First(p => p.NetworkPlayer == Network.player);
+            var ped = this.PedController.Peds.First(p => p.Id == player.PedId);
+            var current = Camera.main.transform.position;
+            var target = new Vector3(
+                Mathf.SmoothStep(current.x, ped.Transform.position.x, 0.1f),
+                Mathf.SmoothStep(current.y, ped.Transform.position.y, 0.1f),
+                current.z
+                );
+            Camera.main.transform.position = target;
+        }
+    }
+}
