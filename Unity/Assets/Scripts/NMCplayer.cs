a5a67698134bf069e7b8fca7f494d011cea17780
Peter Madsen <peter_f_madsen@hotmail.com>
2014-02-15 13:55:55 +0100

    Partial network message controller for the player

---
+++ Unity/Assets/Scripts/NMCplayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+partial class NetworkMessageController : BaseMonoBehaviour
+{
+    [RPC]
+    public void UpdatePlayerDirection(int id, Vector3 direction)
+    {
+        print("Recieved Update Player Direction");
+        if (Network.isServer)
+        {
+            this.PedController.UpdatePedFromClient(id, direction);
+        }
+    }
+}
+
